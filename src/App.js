4ecff7466244bf9af7dccfffc8d27054f6bfdeff
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-03-28 23:58:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+const URL = 'https://jsonplaceholder.typicode.com/photos';
+const [photos, setPhotos] = useState([]);
+
+
+
+useEffect(() => {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => setPhotos(data.slice(0, 20)));
+}, []);
+
+console.log(photos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="card-list">
+        {photos.map((photo) => (
+          <li className="card" key={photo.id}>
+            <img key={photo.id} src={photo.url} alt={photo.title} />
+  
+            <div className="card__info"> 
+              <p>{photo.title}</p>
+            </div>
+            
+
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
