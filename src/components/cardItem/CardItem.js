a66898a7c2250ef33466d182e2aaf565d4f4419e
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-03-29 01:02:13 -0600

    refactoring CardItem

--- src/components/cardItem/CardItem.js
+++ src/components/cardItem/CardItem.js
@@ -2,10 +2,22 @@ import classes from './CardItem.module.css';
 
 const CardItem = (props) => {
   return (
+    // <li className={classes.card} key={props.id}>
+    //    <img key={props.id} src={props.url} alt={props.title} />
+    //    <div className={classes.card__inf}> 
+    //      <p>{props.title}</p>
+    //    </div>
+    // </li>
+
     <li className={classes.card} key={props.id}>
-       <img key={props.id} src={props.url} alt={props.title} />
-       <div className={classes.card__inf}> 
-         <p>{props.title}</p>
+       <div className={classes.card__info}> 
+
+        <img key={props.id} src={props.url} alt={props.title} />
+
+        <div>
+          <p>{props.title}</p>
+        </div>
+
        </div>
     </li>
   );
